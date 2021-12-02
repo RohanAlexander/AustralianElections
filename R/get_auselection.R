6591c324d7f90eead09746256ef0e255712c7608
PAUL A. HODGETTS <paul.hodgetts@mail.utoronto.ca>
2021-12-02 10:25:50 -0500

    Created function to download and filter data according to given year.

--- R/get_auselection.R
+++ R/get_auselection.R
@@ -61,8 +61,10 @@
 
 # assign function name `get_auselection`
 get_auselection <- function(dsr, opr = "", year = 0){
+
   # create vector to hold acceptable `opr` values
   relops <- c("==", ">=", "<=", "<", ">", "")
+
   # assign download URLs vector, these are used to download the data files in a .csv format
   dwnlds <- c("https://raw.github.com/RohanAlexander/australian_federal_elections/master/outputs/byelections.csv",
               "https://raw.github.com/RohanAlexander/australian_federal_elections/master/outputs/elections.csv",
@@ -71,60 +73,78 @@ get_auselection <- function(dsr, opr = "", year = 0){
               "https://raw.github.com/RohanAlexander/australian_federal_elections/master/outputs/voting_data_with_ids.csv"
 
   )
+
   # assign a tibble to hold the request codes used as arguments
   codetibble <- tibble::tibble(
     request_code = c("byelections", "elections", "parliaments", "voting_data", "voting_data_with_ids", "codes"),
     dataset = c("Byelection data", "Election data", "Parliament data", "Voting data", "Voting with IDs data", "Request codes")
   )
+
   # set first error for if dsr is not a character string
   if(!purrr::is_character(dsr)){
     stop("Provided request code must be a character string.")
   }
+
   # set error for if year is not a numeric value
   else if(!is.numeric(year)){
     stop("Year must be a numeric value greater than 0.")
   }
+
   # set error for when passed an incorrect relational operator
   else if(!(opr %in% relops)){
     stop("Provided value for `opr` is not a valid relational operator. Please use one of '==', '>=', '<=', '>', or '<'.")
   }
+
   # else if provided character string is in request_code column values
   else if(dsr %in% codetibble$request_code){
     # assign to temporary file with .csv file type
     tmpdir <- tempfile(fileext = ".csv")
+
     # return request code arguments if given "codes" as argument
     if(dsr == "codes"){
       # show first five values of codetibble
       utils::head(codetibble, 5)
     }
+
     # if `dsr` is equal to "byelections"
     else if(dsr == "byelections"){
+
       # download from the assigned URL to the temporary directory, showing download progress
       utils::download.file(dwnlds[[1]], tmpdir, quiet = F)
+
+
       # if year is left at default value of 0
       if(year == 0){
         # read in complete CSV using `read_csv` from `readr`, do not show column types
         readr::read_csv(tmpdir, show_col_types = F)
       }
+
       # else if year does not equal default value
       else if(year != 0){
+
         # read in CSV and filter for the requested year
         x <- readr::read_csv(tmpdir, show_col_types = F)
+
         # if opr is set to specific relational operator
         if(opr == "==" | opr == ""){
+
           # filter dataset using given relational operator
           # the rest of the code follows similarly
           dplyr::filter(x, lubridate::year(x$date) == year)
         }
+
         else if(opr == ">="){
           dplyr::filter(x, lubridate::year(x$date) >= year)
         }
+
         else if(opr == "<="){
           dplyr::filter(x, lubridate::year(x$date) <= year)
         }
+
         else if(opr == "<"){
           dplyr::filter(x, lubridate::year(x$date) < year)
         }
+
         else if(opr == ">"){
           dplyr::filter(x, lubridate::year(x$date) > year)
         }
@@ -132,101 +152,134 @@ get_auselection <- function(dsr, opr = "", year = 0){
     }
     else if(dsr == "elections"){
       utils::download.file(dwnlds[[2]], tmpdir, quiet = F)
+
       if(year == 0){
         readr::read_csv(tmpdir, show_col_types = F)
       }
+
       else if(year != 0){
         x <- readr::read_csv(tmpdir, show_col_types = F)
+
         if(opr == "==" | opr == ""){
           dplyr::filter(x, lubridate::year(x$electionDate) == year)
         }
+
         else if(opr == ">="){
           dplyr::filter(x, lubridate::year(x$electionDate) >= year)
         }
+
         else if(opr == "<="){
           dplyr::filter(x, lubridate::year(x$electionDate) <= year)
         }
+
         else if(opr == ">"){
           dplyr::filter(x, lubridate::year(x$electionDate) > year)
         }
+
         else if(opr == "<"){
           dplyr::filter(x, lubridate::year(x$electionDate) < year)
         }
       }
     }
     else if(dsr == "parliaments"){
+
       utils::download.file(dwnlds[[3]], tmpdir, quiet = F)
+
       if(year == 0){
         readr::read_csv(tmpdir, show_col_types = F)
       }
+
       else if(year != 0){
+
         x <- readr::read_csv(tmpdir, show_col_types = F)
+
         if(opr == "==" | opr == ""){
           dplyr::filter(x, lubridate::year(x$electionDate) == year)
         }
+
         else if(opr == ">="){
           dplyr::filter(x, lubridate::year(x$electionDate) >= year)
         }
+
         else if(opr == "<="){
           dplyr::filter(x, lubridate::year(x$electionDate) <= year)
         }
+
         else if(opr == ">"){
           dplyr::filter(x, lubridate::year(x$electionDate) > year)
         }
+
         else if(opr == "<"){
           dplyr::filter(x, lubridate::year(x$electionDate) < year)
         }
       }
     }
+
     else if(dsr == "voting_data"){
       utils::download.file(dwnlds[[4]], tmpdir, quiet = F)
+
       if(year == 0){
         readr::read_csv(tmpdir, show_col_types = F)
       }
+
       else if(year != 0){
         x <- readr::read_csv(tmpdir, show_col_types = F)
+
         if(opr == "==" | opr == ""){
           dplyr::filter(x, lubridate::year(x$election_date) == year)
         }
+
         else if(opr == ">="){
           dplyr::filter(x, lubridate::year(x$election_date) >= year)
         }
+
         else if(opr == "<="){
           dplyr::filter(x, lubridate::year(x$election_date) <= year)
         }
+
         else if(opr == ">"){
           dplyr::filter(x, lubridate::year(x$election_date) > year)
         }
+
         else if(opr == "<"){
           dplyr::filter(x, lubridate::year(x$election_date) < year)
         }
       }
     }
+
     else if(dsr == "voting_data_with_ids"){
       utils::download.file(dwnlds[[5]], tmpdir, quiet = F)
+
       if(year == 0){
         readr::read_csv(tmpdir, show_col_types = F)
       }
+
       else if(year != 0){
         x <- readr::read_csv(tmpdir, show_col_types = F)
+
         if(opr == "==" | opr == ""){
           dplyr::filter(x, lubridate::year(x$election_date) == year)
         }
+
         else if(opr == ">="){
           dplyr::filter(x, lubridate::year(x$election_date) >= year)
         }
+
         else if(opr == "<="){
           dplyr::filter(x, lubridate::year(x$election_date) <= year)
         }
+
         else if(opr == ">"){
           dplyr::filter(x, lubridate::year(x$election_date) > year)
         }
+
         else if(opr == "<"){
           dplyr::filter(x, lubridate::year(x$election_date) < year)
         }
       }
     }
   }
+
   else{
     # else character string is not in tibble values stop function and return error
     stop("Provided request code is not associated with a dataset. Please use get_auselections('codes') to print out a set of useable request codes.")
